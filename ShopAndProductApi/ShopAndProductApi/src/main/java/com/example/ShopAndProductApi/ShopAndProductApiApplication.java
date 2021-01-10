b31905cb2f2dede6cbfe4028e91ddbd2f61d78b5
Hassan Harajly <hassanmharajly@hotmail.com>
2021-01-10 12:57:20 -0500

    Add files via upload

---
+++ ShopAndProductApi/ShopAndProductApi/src/main/java/com/example/ShopAndProductApi/ShopAndProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopAndProductApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAndProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAndProductApiApplication.class, args);
+	}
+
+}
---
+++ ShopAndProductApi/ShopAndProductApi/src/test/java/com/example/ShopAndProductApi/ShopAndProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopAndProductApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAndProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
