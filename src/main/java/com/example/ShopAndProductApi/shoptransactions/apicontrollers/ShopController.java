88c8e1b2de6508ba3766954866e3dbb8a6220358
unknown <hassanmharajly@hotmail.com>
2021-02-21 18:56:18 -0500

    hharajl1 whitelisting 4200

--- src/main/java/com/example/ShopAndProductApi/shoptransactions/apicontrollers/ShopController.java
+++ src/main/java/com/example/ShopAndProductApi/shoptransactions/apicontrollers/ShopController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://localhost:4200")
 @RequestMapping("/api/v1/")
 @RestController
 public class ShopController {
